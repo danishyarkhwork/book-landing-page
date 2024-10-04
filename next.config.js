c328db660d07aa24b45380b80373e7163c409892
danishyarkhwork <danishyarkhwork@gmail.com>
2024-10-04 22:08:54 +0430

    fix: the next.config file fixed

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  images: {
-    domains: ["cdn.prod.website-files.com"],
-  },
 };
 
 module.exports = nextConfig;
